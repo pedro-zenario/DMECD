e7ac5a2e0821809961fbe5e7aaed4f1b42dc6948
pedro-zenario <pedrozenario@gmail.com>
2023-06-14 19:56:45 +0100

    adding tetst.py

---
+++ test.py
@@ -0,0 +1,76 @@
+import argparse
+import easydict
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import load_model
+from matplotlib import pyplot as plt
+from plotread import *
+import shap
+# import pickle
+from collections import Counter
+
+def main():
+    ###########################################################################
+    # Parser Definition
+    ###########################################################################
+    opt = easydict.EasyDict({
+        "model": "GRU",
+        "datapath": "Dataset1/",
+        "savepath": "Experiment1/GRU/8/Flatten/Runx/",
+        "extension": ".dat",
+        "batch_size": 32,
+        "plots_in": False,
+        "plots_out": True
+    })
+
+    ###########################################################################
+    # Variables Definition
+    ###########################################################################
+    nin = ['time', 'PLML2', 'PLMR', 'AVBL', 'AVBR']
+    nout = ['time', 'DB1', 'LUAL', 'PVR', 'VB1']
+    neurons = ['time','DB1','LUAL','PVR','VB1','PLML2','PLMR','AVBL','AVBR']
+
+    ###########################################################################
+    # Read data
+    ###########################################################################
+    files = getdata(opt.datapath, opt.extension)
+    train, valid, test = splitdata(files)
+    trainx, trainy = readdata(opt.datapath, train, neurons, nin, nout)
+    validx, validy = readdata(opt.datapath, valid, neurons, nin, nout)
+    testx, testy = readdata(opt.datapath, test, neurons, nin, nout)
+    if opt.plots_in:
+        plotdata(trainx, '/train_data', '/x', opt.model, opt.savepath)
+        plotdata(trainy, '/train_data', '/y', opt.model, opt.savepath)
+        plotdata(validx, '/valid_data', '/x', opt.model, opt.savepath)
+        plotdata(validy, '/valid_data', '/y', opt.model, opt.savepath)
+        plotdata(testx, '/test_data', '/x', opt.model, opt.savepath)
+        plotdata(testy, '/test_data', '/y', opt.model, opt.savepath)
+        
+    ###########################################################################
+    # Load Model and Evaluate
+    ###########################################################################
+    output_size = 4
+    global model
+    model = load_model(opt.savepath + 'model.h5')
+    model.summary()
+
+    print("Starting to explain...")
+    X_train = np.array(trainx)
+    X_test = np.array(testx)
+
+    nin_names = nin[1:len(nin)]
+
+    return X_train, X_test, nin_names
+
+
+X_train, X_test, nin_names = main()
+
+background = X_train[np.random.choice(X_train.shape[0], 1, replace=False)]
+
+explainer = shap.DeepExplainer(model, background)
+
+shap_values = explainer.shap_values(X_test[:1,:,:])
+
+print(shap_values)
