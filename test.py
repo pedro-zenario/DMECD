2554ca75b93769127ad1b169631e47b1d2ccfac2
pedro-zenario <pedrozenario@gmail.com>
2023-06-20 14:09:55 +0100

    Updating test.py v5

--- test.py
+++ test.py
@@ -508,8 +508,56 @@ from tensorflow.python.ops.numpy_ops import np_config
 np_config.enable_numpy_behavior()
 
 def main():
-    # Rest of the code remains the same
-    
+    ###########################################################################
+    # Parser Definition
+    ###########################################################################
+    opt = easydict.EasyDict({
+        "model": "GRU",
+        "datapath": "Dataset1/",
+        "savepath": "Experiment1/GRU/8/Flatten/Runx/",
+        "extension": ".dat",
+        "batch_size": 32,
+        "plots_in": False,
+        "plots_out": True
+    })
+
+    ###########################################################################
+    # Variables Definition
+    ###########################################################################
+    nin = ['time', 'PLML2', 'PLMR', 'AVBL', 'AVBR']
+    nout = ['time', 'DB1', 'LUAL', 'PVR', 'VB1']
+    neurons = ['time','DB1','LUAL','PVR','VB1','PLML2','PLMR','AVBL','AVBR']
+
+    ###########################################################################
+    # Read data
+    ###########################################################################
+    files = getdata(opt.datapath, opt.extension)
+    train, valid, test = splitdata(files)
+    trainx, trainy = readdata(opt.datapath, train, neurons, nin, nout)
+    validx, validy = readdata(opt.datapath, valid, neurons, nin, nout)
+    testx, testy = readdata(opt.datapath, test, neurons, nin, nout)
+    if opt.plots_in:
+        plotdata(trainx, '/train_data', '/x', opt.model, opt.savepath)
+        plotdata(trainy, '/train_data', '/y', opt.model, opt.savepath)
+        plotdata(validx, '/valid_data', '/x', opt.model, opt.savepath)
+        plotdata(validy, '/valid_data', '/y', opt.model, opt.savepath)
+        plotdata(testx, '/test_data', '/x', opt.model, opt.savepath)
+        plotdata(testy, '/test_data', '/y', opt.model, opt.savepath)
+        
+    ###########################################################################
+    # Load Model and Evaluate
+    ###########################################################################
+    output_size = 4
+    global model
+    model = load_model(opt.savepath + 'model.h5')
+    model.summary()
+
+    print("Starting to explain...")
+    X_train = np.array(trainx)
+    X_test = np.array(testx)
+
+    nin_names = nin[1:len(nin)]
+
     return X_train, X_test, nin_names
 
 def calculate_shap_values(model, X_test, background):
