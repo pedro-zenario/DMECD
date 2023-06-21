9787e711b2fa7b748ec6b9bd0521cf3771a5789e
pedro-zenario <pedrozenario@gmail.com>
2023-06-21 10:57:25 +0100

    test.py original

--- test.py
+++ test.py
@@ -1,90 +1,90 @@
-# import argparse
-# import easydict
-# import numpy as np
-# import pandas as pd
-# import tensorflow as tf
-# from tensorflow import keras
-# from tensorflow.keras.models import load_model
-# from matplotlib import pyplot as plt
-# from plotread import *
-# import shap
-# import pickle
-# import pickle
-# from collections import Counter
+import argparse
+import easydict
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import load_model
+from matplotlib import pyplot as plt
+from plotread import *
+import shap
+import pickle
+import pickle
+from collections import Counter
 
-# import tensorflow._api.v2.compat.v1 as tf
-# from tensorflow.compat.v1.keras.backend import get_session
-# tf.compat.v1.disable_v2_behavior()
-# tf.compat.v1.disable_eager_execution()
-# from tensorflow.python.ops.numpy_ops import np_config
-# np_config.enable_numpy_behavior()
+import tensorflow._api.v2.compat.v1 as tf
+from tensorflow.compat.v1.keras.backend import get_session
+tf.compat.v1.disable_v2_behavior()
+tf.compat.v1.disable_eager_execution()
+from tensorflow.python.ops.numpy_ops import np_config
+np_config.enable_numpy_behavior()
 
-# def main():
-#     ###########################################################################
-#     # Parser Definition
-#     ###########################################################################
-#     opt = easydict.EasyDict({
-#         "model": "GRU",
-#         "datapath": "Dataset1/",
-#         "savepath": "Experiment1/GRU/8/Flatten/Runx/",
-#         "extension": ".dat",
-#         "batch_size": 32,
-#         "plots_in": False,
-#         "plots_out": True
-#     })
+def main():
+    ###########################################################################
+    # Parser Definition
+    ###########################################################################
+    opt = easydict.EasyDict({
+        "model": "GRU",
+        "datapath": "Dataset1/",
+        "savepath": "Experiment1/GRU/8/Flatten/Runx/",
+        "extension": ".dat",
+        "batch_size": 32,
+        "plots_in": False,
+        "plots_out": True
+    })
 
-#     ###########################################################################
-#     # Variables Definition
-#     ###########################################################################
-#     nin = ['time', 'PLML2', 'PLMR', 'AVBL', 'AVBR']
-#     nout = ['time', 'DB1', 'LUAL', 'PVR', 'VB1']
-#     neurons = ['time','DB1','LUAL','PVR','VB1','PLML2','PLMR','AVBL','AVBR']
+    ###########################################################################
+    # Variables Definition
+    ###########################################################################
+    nin = ['time', 'PLML2', 'PLMR', 'AVBL', 'AVBR']
+    nout = ['time', 'DB1', 'LUAL', 'PVR', 'VB1']
+    neurons = ['time','DB1','LUAL','PVR','VB1','PLML2','PLMR','AVBL','AVBR']
 
-#     ###########################################################################
-#     # Read data
-#     ###########################################################################
-#     files = getdata(opt.datapath, opt.extension)
-#     train, valid, test = splitdata(files)
-#     trainx, trainy = readdata(opt.datapath, train, neurons, nin, nout)
-#     validx, validy = readdata(opt.datapath, valid, neurons, nin, nout)
-#     testx, testy = readdata(opt.datapath, test, neurons, nin, nout)
-#     if opt.plots_in:
-#         plotdata(trainx, '/train_data', '/x', opt.model, opt.savepath)
-#         plotdata(trainy, '/train_data', '/y', opt.model, opt.savepath)
-#         plotdata(validx, '/valid_data', '/x', opt.model, opt.savepath)
-#         plotdata(validy, '/valid_data', '/y', opt.model, opt.savepath)
-#         plotdata(testx, '/test_data', '/x', opt.model, opt.savepath)
-#         plotdata(testy, '/test_data', '/y', opt.model, opt.savepath)
+    ###########################################################################
+    # Read data
+    ###########################################################################
+    files = getdata(opt.datapath, opt.extension)
+    train, valid, test = splitdata(files)
+    trainx, trainy = readdata(opt.datapath, train, neurons, nin, nout)
+    validx, validy = readdata(opt.datapath, valid, neurons, nin, nout)
+    testx, testy = readdata(opt.datapath, test, neurons, nin, nout)
+    if opt.plots_in:
+        plotdata(trainx, '/train_data', '/x', opt.model, opt.savepath)
+        plotdata(trainy, '/train_data', '/y', opt.model, opt.savepath)
+        plotdata(validx, '/valid_data', '/x', opt.model, opt.savepath)
+        plotdata(validy, '/valid_data', '/y', opt.model, opt.savepath)
+        plotdata(testx, '/test_data', '/x', opt.model, opt.savepath)
+        plotdata(testy, '/test_data', '/y', opt.model, opt.savepath)
         
-#     ###########################################################################
-#     # Load Model and Evaluate
-#     ###########################################################################
-#     output_size = 4
-#     global model
-#     model = load_model(opt.savepath + 'model.h5')
-#     model.summary()
+    ###########################################################################
+    # Load Model and Evaluate
+    ###########################################################################
+    output_size = 4
+    global model
+    model = load_model(opt.savepath + 'model.h5')
+    model.summary()
 
-#     print("Starting to explain...")
-#     X_train = np.array(trainx)
-#     X_test = np.array(testx)
+    print("Starting to explain...")
+    X_train = np.array(trainx)
+    X_test = np.array(testx)
 
-#     nin_names = nin[1:len(nin)]
+    nin_names = nin[1:len(nin)]
 
-#     return X_train, X_test, nin_names
+    return X_train, X_test, nin_names
 
-# if __name__ == '__main__':
-#     X_train, X_test, nin_names = main()
+if __name__ == '__main__':
+    X_train, X_test, nin_names = main()
 
-#     background = X_train[np.random.choice(X_train.shape[0], 1, replace=False)]
+    background = X_train[np.random.choice(X_train.shape[0], 1, replace=False)]
 
-#     explainer = shap.DeepExplainer(model, background)
+    explainer = shap.DeepExplainer(model, background)
 
-#     shap_values = explainer.shap_values(X_test[:1,:,:])
+    shap_values = explainer.shap_values(X_test[:1,:,:])
 
-#     print(shap_values)
+    print(shap_values)
 
-#     with open("shap_values.pkl", 'wb') as file:
-#         pickle.dump(shap_values, file)
+    with open("shap_values.pkl", 'wb') as file:
+        pickle.dump(shap_values, file)
 
 
 
@@ -214,117 +214,117 @@
 
 
 
-import argparse
-import easydict
-import numpy as np
-import pandas as pd
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras.models import load_model
-from matplotlib import pyplot as plt
-from plotread import *
-import shap
-import pickle
-from collections import Counter
-from multiprocessing import Pool
+# import argparse
+# import easydict
+# import numpy as np
+# import pandas as pd
+# import tensorflow as tf
+# from tensorflow import keras
+# from tensorflow.keras.models import load_model
+# from matplotlib import pyplot as plt
+# from plotread import *
+# import shap
+# import pickle
+# from collections import Counter
+# from multiprocessing import Pool
 
-import tensorflow._api.v2.compat.v1 as tf
-from tensorflow.compat.v1.keras.backend import get_session
-tf.compat.v1.disable_v2_behavior()
-tf.compat.v1.disable_eager_execution()
-from tensorflow.python.ops.numpy_ops import np_config
-np_config.enable_numpy_behavior()
+# import tensorflow._api.v2.compat.v1 as tf
+# from tensorflow.compat.v1.keras.backend import get_session
+# tf.compat.v1.disable_v2_behavior()
+# tf.compat.v1.disable_eager_execution()
+# from tensorflow.python.ops.numpy_ops import np_config
+# np_config.enable_numpy_behavior()
 
-def main():
-    ###########################################################################
-    # Parser Definition
-    ###########################################################################
-    opt = easydict.EasyDict({
-        "model": "GRU",
-        "datapath": "Dataset1/",
-        "savepath": "Experiment1/GRU/8/Flatten/Runx/",
-        "extension": ".dat",
-        "batch_size": 32,
-        "plots_in": False,
-        "plots_out": True
-    })
+# def main():
+#     ###########################################################################
+#     # Parser Definition
+#     ###########################################################################
+#     opt = easydict.EasyDict({
+#         "model": "GRU",
+#         "datapath": "Dataset1/",
+#         "savepath": "Experiment1/GRU/8/Flatten/Runx/",
+#         "extension": ".dat",
+#         "batch_size": 32,
+#         "plots_in": False,
+#         "plots_out": True
+#     })
 
-    ###########################################################################
-    # Variables Definition
-    ###########################################################################
-    nin = ['time', 'PLML2', 'PLMR', 'AVBL', 'AVBR']
-    nout = ['time', 'DB1', 'LUAL', 'PVR', 'VB1']
-    neurons = ['time','DB1','LUAL','PVR','VB1','PLML2','PLMR','AVBL','AVBR']
+#     ###########################################################################
+#     # Variables Definition
+#     ###########################################################################
+#     nin = ['time', 'PLML2', 'PLMR', 'AVBL', 'AVBR']
+#     nout = ['time', 'DB1', 'LUAL', 'PVR', 'VB1']
+#     neurons = ['time','DB1','LUAL','PVR','VB1','PLML2','PLMR','AVBL','AVBR']
 
-    ###########################################################################
-    # Read data
-    ###########################################################################
-    files = getdata(opt.datapath, opt.extension)
-    train, valid, test = splitdata(files)
-    trainx, trainy = readdata(opt.datapath, train, neurons, nin, nout)
-    validx, validy = readdata(opt.datapath, valid, neurons, nin, nout)
-    testx, testy = readdata(opt.datapath, test, neurons, nin, nout)
-    if opt.plots_in:
-        plotdata(trainx, '/train_data', '/x', opt.model, opt.savepath)
-        plotdata(trainy, '/train_data', '/y', opt.model, opt.savepath)
-        plotdata(validx, '/valid_data', '/x', opt.model, opt.savepath)
-        plotdata(validy, '/valid_data', '/y', opt.model, opt.savepath)
-        plotdata(testx, '/test_data', '/x', opt.model, opt.savepath)
-        plotdata(testy, '/test_data', '/y', opt.model, opt.savepath)
+#     ###########################################################################
+#     # Read data
+#     ###########################################################################
+#     files = getdata(opt.datapath, opt.extension)
+#     train, valid, test = splitdata(files)
+#     trainx, trainy = readdata(opt.datapath, train, neurons, nin, nout)
+#     validx, validy = readdata(opt.datapath, valid, neurons, nin, nout)
+#     testx, testy = readdata(opt.datapath, test, neurons, nin, nout)
+#     if opt.plots_in:
+#         plotdata(trainx, '/train_data', '/x', opt.model, opt.savepath)
+#         plotdata(trainy, '/train_data', '/y', opt.model, opt.savepath)
+#         plotdata(validx, '/valid_data', '/x', opt.model, opt.savepath)
+#         plotdata(validy, '/valid_data', '/y', opt.model, opt.savepath)
+#         plotdata(testx, '/test_data', '/x', opt.model, opt.savepath)
+#         plotdata(testy, '/test_data', '/y', opt.model, opt.savepath)
         
-    ###########################################################################
-    # Load Model and Evaluate
-    ###########################################################################
-    output_size = 4
-    global model
-    model = load_model(opt.savepath + 'model.h5')
-    model.summary()
+#     ###########################################################################
+#     # Load Model and Evaluate
+#     ###########################################################################
+#     output_size = 4
+#     global model
+#     model = load_model(opt.savepath + 'model.h5')
+#     model.summary()
 
-    print("Starting to explain...")
-    X_train = np.array(trainx)
-    X_test = np.array(testx)
+#     print("Starting to explain...")
+#     X_train = np.array(trainx)
+#     X_test = np.array(testx)
 
-    nin_names = nin[1:len(nin)]
+#     nin_names = nin[1:len(nin)]
 
-    return X_train, X_test, nin_names
-
-
-def run_shap(X_test, model, background):
-    print("Inside run_shap")
-    explainer = shap.DeepExplainer(model, background)
-    print("Middle run_shap")
-    shap_values = explainer.shap_values(X_test[:, :, :])
-    print("Returning run_shap")
-    return shap_values
+#     return X_train, X_test, nin_names
 
 
-if __name__ == '__main__':
-
-    X_train, X_test, nin_names = main()
-
-    background = X_train[np.random.choice(X_train.shape[0], 1, replace=False)]
-
-    # Define the number of processes to use
-    num_processes = 128  # Adjust this number based on your system's capacity
-
-    # Create a Pool of processes
-    pool = Pool(processes=num_processes)
-
-    # Split the X_test data into chunks for parallel processing
-    chunk_size = len(X_test) // num_processes
-    print(chunk_size)
-    X_test_chunks = [X_test[i:i+chunk_size, :, :] for i in range(0, len(X_test), chunk_size)]
-    print(X_test_chunks)
-    print(np.array(X_test_chunks).shape)
+# def run_shap(X_test, model, background):
+#     print("Inside run_shap")
+#     explainer = shap.DeepExplainer(model, background)
+#     print("Middle run_shap")
+#     shap_values = explainer.shap_values(X_test[:, :, :])
+#     print("Returning run_shap")
+#     return shap_values
 
 
-    # Execute the main function in parallel
-    results = pool.map(run_shap(X_test, model, background), X_test_chunks)
+# if __name__ == '__main__':
 
-    # Combine the results
-    combined_results = np.concatenate(results, axis=0)
+#     X_train, X_test, nin_names = main()
 
-    print(combined_results)
+#     background = X_train[np.random.choice(X_train.shape[0], 1, replace=False)]
 
-    with open("shap_values.pkl", 'wb') as file:
-        pickle.dump(combined_results, file)
+#     # Define the number of processes to use
+#     num_processes = 10  # Adjust this number based on your system's capacity
+
+#     # Create a Pool of processes
+#     pool = Pool(processes=num_processes)
+
+#     # Split the X_test data into chunks for parallel processing
+#     chunk_size = len(X_test) // num_processes
+#     print(chunk_size)
+#     X_test_chunks = [X_test[i:i+chunk_size, :, :] for i in range(0, len(X_test), chunk_size)]
+#     print(X_test_chunks)
+#     print(np.array(X_test_chunks).shape)
+
+
+#     # Execute the main function in parallel
+#     results = pool.map(run_shap(X_test, model, background), X_test_chunks)
+
+#     # Combine the results
+#     combined_results = np.concatenate(results, axis=0)
+
+#     print(combined_results)
+
+#     with open("shap_values.pkl", 'wb') as file:
+#         pickle.dump(combined_results, file)
